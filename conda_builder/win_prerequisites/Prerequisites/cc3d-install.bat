2bca5a506dec452b5452cf07604ea4cd39a724b4
compucell3d <compucell3dproject@gmail.com>
2022-07-09 14:04:10 -0600

    implemented logging support during installation and eliminated the need to press any key at the en of installation

--- conda_builder/win_prerequisites/Prerequisites/cc3d-install.bat
+++ conda_builder/win_prerequisites/Prerequisites/cc3d-install.bat
@@ -3,16 +3,8 @@ echo "WILL INSTALL COMPUCELL3D VERSION =" %version%
 SET script_dir=%~dp0
 @CALL %script_dir%\..\Miniconda3\condabin\activate.bat
 
-REM unzipping demos
-rem %script_dir%\..\Miniconda3\python -m zipfile -e %script_dir%\..\Demos.zip %script_dir%\..\Demos
-
-rem installing actual compucell3d
-
-rem conda install -y -c compucell3d -c conda-forge compucell3d=%version% >  %script_dir%\install.log & type install.log
-pause
-tee --help
-pause
+rem this requires that we bundle tee command (tee.exe) with the installer
 set PATH= %script_dir%\..\Prerequisites;%PATH%
 conda install -y -c compucell3d -c conda-forge compucell3d=%version% |tee install.log 2>&1
-pause
+
 
