2e8dedbb05c84ebe17491bf401ae0599e95b4aa8
maciekswat <maciekswat@gmail.com>
2023-03-04 12:14:18 -0700

    adding improvements to build rr-antimony conda on linux - had to include links

--- conda-recipes/roadrunner_antimony/conda-recipe/build.sh
+++ conda-recipes/roadrunner_antimony/conda-recipe/build.sh
@@ -20,5 +20,9 @@ for val in "${StringArray[@]}"; do
   cp -r $val ${SP_DIR}
 done
 
+# setting up links for linux distros - commend for mac
+ls -s /home/m/miniconda3/envs/rr_${python_version}/lib/libncurses.so /home/m/miniconda3/envs/rr_${python_version}/lib/${python_version}/site-packages/roadrunner/libncurses.so.5
+ls -s /home/m/miniconda3/envs/rr_${python_version}/lib/libtinfo.so /home/m/miniconda3/envs/rr_${python_version}/lib/${python_version}/site-packages/roadrunner/libtinfo.so.5
+ls -s /home/m/miniconda3/envs/rr_${python_version}/lib/libform.so /home/m/miniconda3/envs/rr_${python_version}/lib/${python_version}/site-packages/roadrunner/libform.so.5
 #cp -R /Users/m/tbb/* "${PREFIX}"
 ##$PYTHON setup.py install --single-version-externally-managed --record=record.txt
