34579af22abf914fb5465f81d33bfd88f916c94f
maciekswat <maciekswat@gmail.com>
2023-03-11 13:55:06 -0700

    added shel script to buil entire cc3d suite

--- conda_builder/build_cc3d_suite.bat
+++ conda_builder/build_cc3d_suite.bat
@@ -4,16 +4,8 @@ set player_repo_dir=d:\src\conda-build-repos\cc3d-player5\
 set twedit_repo_dir=d:\src\conda-build-repos\cc3d-twedit5\
 set SCRIPT_DIR=%~dp0
 
-echo %SCRIPT_DIR%
-@REM build cc3d
 
-@REM cd %cc3d_repo_dir%\conda-recipes
-@REM move %cc3d_repo_dir%\conda-recipes\versions.yaml versions.yaml.bak
-@REM copy  %SCRIPT_DIR%\versions.yaml %cc3d_repo_dir%\conda-recipes\versions.yaml
-@REM move %cc3d_repo_dir%\conda-recipes\versions.yaml.bak versions.yaml
-@REM cd %SCRIPT_DIR%
-
-@REM call :build_conda_package %cc3d_repo_dir%\conda-recipes
+call :build_conda_package %cc3d_repo_dir%\conda-recipes
 call :build_conda_package %player_repo_dir%\conda-recipes
 call :build_conda_package %twedit_repo_dir%\conda-recipes
 call :build_conda_package %cc3d_repo_dir%\conda-recipes-compucell3d
