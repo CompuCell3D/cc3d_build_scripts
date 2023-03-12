cbcfa0fc4d64319ebc52686161045545cd90e24b
Maciek Swat <maciekswat@gmail.com>
2023-03-11 18:08:20 -0700

    fixing scripts for building binariess for linux

--- conda_builder/build_cc3d_suite.sh
+++ conda_builder/build_cc3d_suite.sh
@@ -2,9 +2,11 @@
 cc3d_repo_dir=/home/m/src/conda-build-repos/CompuCell3D/
 player_repo_dir=/home/m/src/conda-build-repos/cc3d-player5/
 twedit_repo_dir=/home/m/src/conda-build-repos/cc3d-twedit5/
-SCRIPT_DIR=$(dirname "$0")
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# SCRIPT_DIR=$(dirname "$0")
 
 echo "${SCRIPT_DIR}"
+
 
 function build_conda_package () {
   local conda_recipe_dir=$1
@@ -12,12 +14,12 @@ function build_conda_package () {
   cd "${conda_recipe_dir}" || exit
   mv versions.yaml versions.yaml.bak
   cp  "${SCRIPT_DIR}/versions.yaml" versions.yaml
-  source run_conda_build.sh
+  ./run_conda_build.sh
   mv versions.yaml.bak versions.yaml
 }
 
 
 build_conda_package "${cc3d_repo_dir}/conda-recipes"
-#build_conda_package "${cc3d_player_repo_dir}/conda-recipes"
-#build_conda_package "${twedit_repo_dir}/conda-recipes"
-#build_conda_package "${cc3d_repo_dir}/conda-recipes-compucell3d"
+build_conda_package "${player_repo_dir}/conda-recipes"
+build_conda_package "${twedit_repo_dir}/conda-recipes"
+build_conda_package "${cc3d_repo_dir}/conda-recipes-compucell3d"
