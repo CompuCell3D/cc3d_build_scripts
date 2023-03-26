3a28a6ae211eae7f062ba2ad3f79f5a95ac9b95c
maciekswat <maciekswat@gmail.com>
2023-03-25 19:21:25 -0600

    improved build scripts

--- conda_builder/build_cc3d_suite.bat
+++ conda_builder/build_cc3d_suite.bat
@@ -4,20 +4,29 @@ set player_repo_dir=d:\src\conda-build-repos\cc3d-player5\
 set twedit_repo_dir=d:\src\conda-build-repos\cc3d-twedit5\
 set SCRIPT_DIR=%~dp0
 
+IF "%1"=="" (
+    SET PYTHON_VERSION=3.7
+) ELSE (
+    SET PYTHON_VERSION=%1
+)
 
-call :build_conda_package %cc3d_repo_dir%\conda-recipes
-call :build_conda_package %player_repo_dir%\conda-recipes
-call :build_conda_package %twedit_repo_dir%\conda-recipes
-call :build_conda_package %cc3d_repo_dir%\conda-recipes-compucell3d
+
+echo %PYTHON_VERSION%
+
+call :build_conda_package %cc3d_repo_dir%\conda-recipes %PYTHON_VERSION%
+call :build_conda_package %player_repo_dir%\conda-recipes %PYTHON_VERSION%
+call :build_conda_package %twedit_repo_dir%\conda-recipes %PYTHON_VERSION%
+call :build_conda_package %cc3d_repo_dir%\conda-recipes-compucell3d %PYTHON_VERSION%
 
 exit /b
 
 :build_conda_package
 set conda_recipe_dir=%1
+set py_version=%2
 cd %conda_recipe_dir%
 move versions.yaml versions.yaml.bak
 copy  %SCRIPT_DIR%\versions.yaml versions.yaml
-call run_conda_build.bat
+call run_conda_build.bat %py_version%
 move versions.yaml.bak versions.yaml
 cd %SCRIPT_DIR% 
 
--- conda_builder/build_cc3d_suite.sh
+++ conda_builder/build_cc3d_suite.sh
@@ -3,6 +3,14 @@ if [ "$(uname)" == "Darwin" ]; then
   repo_prefix=/Users/m/src/conda-build-repos
 fi
 
+if [ $# -eq 0 ]; then
+    # If not, assign a default value
+    PYTHON_VERSION=3.7
+else
+    # If yes, use the passed value
+    PYTHON_VERSION="$1"
+fi
+
 
 cc3d_repo_dir=${repo_prefix}/CompuCell3D/
 player_repo_dir=${repo_prefix}/cc3d-player5/
@@ -15,16 +23,16 @@ echo "${SCRIPT_DIR}"
 
 function build_conda_package () {
   local conda_recipe_dir=$1
-
+  local py_version=$2
   cd "${conda_recipe_dir}" || exit
   mv versions.yaml versions.yaml.bak
   cp  "${SCRIPT_DIR}/versions.yaml" versions.yaml
-  ./run_conda_build.sh
+  ./run_conda_build.sh "${py_version}"
   mv versions.yaml.bak versions.yaml
 }
 
 
-build_conda_package "${cc3d_repo_dir}/conda-recipes"
-build_conda_package "${player_repo_dir}/conda-recipes"
-build_conda_package "${twedit_repo_dir}/conda-recipes"
-build_conda_package "${cc3d_repo_dir}/conda-recipes-compucell3d"
+build_conda_package "${cc3d_repo_dir}/conda-recipes" "${PYTHON_VERSION}"
+build_conda_package "${player_repo_dir}/conda-recipes" "${PYTHON_VERSION}"
+build_conda_package "${twedit_repo_dir}/conda-recipes" "${PYTHON_VERSION}"
+build_conda_package "${cc3d_repo_dir}/conda-recipes-compucell3d" "${PYTHON_VERSION}"
