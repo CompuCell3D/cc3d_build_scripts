f4e0fe107470ddabf4b41f2c091f83a45b61bf4f
Maciek Swat <maciekswat@gmail.com>
2023-04-16 13:35:53 -0600

    added instructions of how to compile CC3D on linux using quasi-manual process

---
+++ linux/nanohub/run_scripts/twedit++.sh
@@ -0,0 +1,10 @@
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+
+export PYTHON_EXEC=/home/m/miniconda3/envs/cc3d_linux_manual_37/bin/python
+export PYTHONPATH=/home/m/miniconda3/envs/cc3d_linux_manual_37/lib/python3.7/site-packages
+export LD_LIBRARY_PATH=/home/m/miniconda3/envs/cc3d_linux_manual_37/lib:$LD_LIBRARY_PATH
+
+
+${PYTHON_EXEC} -m  cc3d.twedit5 $* --currentDir=${SCRIPT_DIR}
